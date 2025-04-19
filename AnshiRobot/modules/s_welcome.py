f631a47e8f87846ba08ea22fd914e3d523e685cf
⫷ Aɳαɳԃ Sԋαɾɱα ⫸ <91930171+BadshahAk@users.noreply.github.com>
2025-04-19 13:25:22 +0530

    Update s_welcome.py

--- AnshiRobot/modules/s_welcome.py
+++ AnshiRobot/modules/s_welcome.py
@@ -119,7 +119,7 @@ async def greet_group(_, member: ChatMemberUpdated):
             member.chat.id,
             photo=welcomeimg,
             caption= f"""
-**ㅤㅤㅤ◦•●◉✿ ᴡᴇʟᴄᴏᴍᴇ ʙᴀʙʏ ✿◉●•◦
+**ㅤㅤㅤ◦•●◉✿ ʀᴀᴅʜᴇ ʀᴀᴅʜᴇ ✿◉●•◦
 ▰▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▰
 
 ● ɢʀᴏᴜᴘ ➥ {member.chat.title}
@@ -127,7 +127,7 @@ async def greet_group(_, member: ChatMemberUpdated):
 ● ᴜsᴇʀ ɪᴅ ➥ {user.id}
 ● ᴜsᴇʀɴᴀᴍᴇ ➥ @{user.username}
 
-❖ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ [๛ᴀ ʟ ɪ s ʜ ᴀ ࿐](https://t.me/the_incricible)**
+❖ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ [๛ᴀ ɴ ꜱ ʜ ɪ ࿐](https://t.me/New_Sanatani)**
 ▰▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▰
 """,
             reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton (f"ᴠɪᴇᴡ ᴜsᴇʀ", url=f"https://t.me/{user.username}")]])
